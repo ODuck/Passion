ed2385f955d40cbb9595ea61d528406dfd618e7c
Repump <ojunhy0@gmail.com>
2016-04-27 23:55:44 +0900

    Test
    File upload test

---
+++ Test.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Test {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int C = sc.nextInt();
+		for (int i = 0; i < C; i++) {
+			// To-do
+		}
+		sc.close();
+	}
+}
